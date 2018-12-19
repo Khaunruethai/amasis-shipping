03f01e171ceb50611ea19d2ebdb66c8294019a1f
Khaunruethai <34817984+Khaunruethai@users.noreply.github.com>
2018-12-19 12:51:52 +0700

    uu

--- fontend/Insert.php
+++ fontend/Insert.php
@@ -1,6 +1,11 @@
 <?php
-   $con = mysqli_connect("us-cdbr-iron-east-01.cleardb.net", "bc31b9f07b0dea", "","heroku_582a87dceadee92");
-   mysqli_set_charset($con, "utf8");
+	$serverName = "us-cdbr-iron-east-01.cleardb.net";
+	$userName = "bc31b9f07b0dea";
+	$userPassword = "5904a8d2";
+	$dbName = "heroku_582a87dceadee92";
+
+  $objCon = mysqli_connect($serverName,$userName,$userPassword,$dbName);
+  mysqli_set_charset($objCon,"utf8");
    
    $id = $_POST["id"];
    $FirstName = $_POST["FirstName"];
@@ -10,7 +15,7 @@
    $Subject = $_POST["Subject"];
    $query = "INSERT INTO contact_us (id,FirstName,LastName,Country,Email,Subject)
    VALUES('','$FirstName','$LastName','$Country','$Email','$Subject')";
-   mysqli_query($con, $query);
+   mysqli_query($objcon,$query);
 
-   header("Location:contact.php");
+   header("Location:./contact.php");
 ?>
