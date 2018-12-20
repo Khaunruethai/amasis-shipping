3465929ff8187891a1d9e116c402bad277cd2678
Khaunruethai <34817984+Khaunruethai@users.noreply.github.com>
2018-12-20 13:33:44 +0700

    tt

--- fontend/contact.php
+++ fontend/contact.php
@@ -114,7 +114,8 @@
           <input type="text" id="Subject" name="Email" placeholder="E-mail.."style="height:35px"><br><br>
           <textarea type="text" id="Subject" name="Subject" placeholder="Write something.."style="width:345px"></textarea><br>
           <div class="row">
-          <div class="col-sm-10">
+          <div class="col-sm-10"></div>
+          <div class="col-sm-2">
           <button type="submit" class="btn btn-primary">sent</button></div></div>
           </form>
           </div>
