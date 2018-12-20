de48f1744f4d1e202edd76c0eb74bd7682292459
Khaunruethai <34817984+Khaunruethai@users.noreply.github.com>
2018-12-20 13:34:48 +0700

    iu

--- fontend/contact.php
+++ fontend/contact.php
@@ -112,15 +112,15 @@
           <input type="text" id="Subject" name="LastName" placeholder="Your last name.."style="height:35px"><br><br>
           <input type="text"id="Subject" name="Country" placeholder="Country.."style="height:35px">
           <input type="text" id="Subject" name="Email" placeholder="E-mail.."style="height:35px"><br><br>
-          <textarea type="text" id="Subject" name="Subject" placeholder="Write something.."style="width:345px"></textarea><br>
+          <textarea type="text" id="Subject" name="Subject" placeholder="Write something.."style="width:345px"></textarea>
+          </div>
+          <div class="col-md-2">
+          </div>
           <div class="row">
           <div class="col-sm-10"></div>
           <div class="col-sm-2">
           <button type="submit" class="btn btn-primary">sent</button></div></div>
           </form>
-          </div>
-          <div class="col-md-2">
-          </div>
           </div>
           </div>
           </div>
